04f3f4427e5900ab164f39aa1ce64626aa17eda0
Yuga Ehara <yehara@c1r1s1.42tokyo.jp>
2024-05-22 22:39:56 +0900

    additional .h file and utils.c

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,27 @@
 /*   By: yehara <yehara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 20:33:26 by yehara            #+#    #+#             */
-/*   Updated: 2024/05/21 20:33:27 by yehara           ###   ########.fr       */
+/*   Updated: 2024/05/22 22:33:48 by yehara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+//lib
+# include <stdio.h>
+
+typedef struct s_string
+{
+	char	*str;
+	size_t	len;
+	size_t	capa;
+}	t_string;
+
+
+//gnl
+
+//gnl_utils
+#endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,39 @@
 /*   By: yehara <yehara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 20:33:20 by yehara            #+#    #+#             */
-/*   Updated: 2024/05/21 20:33:21 by yehara           ###   ########.fr       */
+/*   Updated: 2024/05/22 22:38:15 by yehara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static int	getchar(int fd)
+{
+	static char	buf[BUFFER_SIZE];
+	static char	*bufp;
+	static int	n;
+
+	n = 0;
+	if (n == 0)
+	{
+		n = read(fd, buf, BUFFER_SIZE);
+		if (n == -1)
+			return (-1);
+		bufp = buf;
+	}
+	if (--n >= 0)
+		return ((unsigned char) *bufp++);
+	else
+		return (EOF);
+}
+
+static int	ft_putc(t_string *str, char c)
+{
+	if (str->len + 1 >= str->capa)
+	{
+
+	}
+	str->str[str->len] = c;
+	str->len++;
+	return (0);
+}
